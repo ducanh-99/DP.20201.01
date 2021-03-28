d27e4ee48908332fb71d9007458c8f784c8e432e
ducanh <ducanhhust99@gmail.com>
2021-03-28 20:26:58 +0700

    ducanh - add singleton

---
+++ Contribution.md
@@ -0,0 +1,9 @@
+## Singleton
+
+- Subteam 2 : Nguyen Duc Anh Contribution
+  
+  
+  stt | class | explan
+  --- | --- | --- |
+  1 | Cart| 1 shop chỉ cần 1 cart nên có thể dùng Singleton cho Cart
+  2 | InterbankPayloadConverter | là một lớp ngoài nên chỉ cần Singleton 
--- src/main/java/entity/cart/Cart.java
+++ src/main/java/entity/cart/Cart.java
@@ -8,31 +8,39 @@ import common.exception.MediaNotAvailableException;
 import entity.media.Media;
 
 public class Cart {
-    
-    // Content coupling: CartItem chưa
-    private List<CartItem> lstCartItem;
 
-    public Cart() {
-        lstCartItem = new ArrayList<>();
+    private static Cart instance;
+
+    // Content coupling: CartItem chưa
+    private List<CartItem> lstCartItem = new ArrayList<>();
+
+    private Cart() {
     }
 
-    public void addCartMedia(CartItem cm){
+    public synchronized static Cart getInstanse() {
+        if (instance == null) {
+            instance = new Cart();
+        }
+        return instance;
+    }
+
+    public void addCartMedia(CartItem cm) {
         lstCartItem.add(cm);
     }
 
-    public void removeCartMedia(CartItem cm){
+    public void removeCartMedia(CartItem cm) {
         lstCartItem.remove(cm);
     }
 
-    public List getListMedia(){
+    public List getListMedia() {
         return lstCartItem;
     }
 
-    public void emptyCart(){
+    public void emptyCart() {
         lstCartItem.clear();
     }
 
-    public int getTotalMedia(){
+    public int getTotalMedia() {
         int total = 0;
         for (Object obj : lstCartItem) {
             CartItem cm = (CartItem) obj;
@@ -41,29 +49,32 @@ public class Cart {
         return total;
     }
 
-    public int calSubtotal(){
+    public int calSubtotal() {
         int total = 0;
         for (Object obj : lstCartItem) {
             CartItem cm = (CartItem) obj;
-            total += cm.getPrice()*cm.getQuantity();
+            total += cm.getPrice() * cm.getQuantity();
         }
         return total;
     }
 
-    public void checkAvailabilityOfProduct() throws SQLException{
+    public void checkAvailabilityOfProduct() throws SQLException {
         boolean allAvailable = true;
         for (Object object : lstCartItem) {
             CartItem cartItem = (CartItem) object;
             int requiredQuantity = cartItem.getQuantity();
             int availQuantity = cartItem.getMedia().getQuantity();
-            if (requiredQuantity > availQuantity) allAvailable = false;
+            if (requiredQuantity > availQuantity)
+                allAvailable = false;
         }
-        if (!allAvailable) throw new MediaNotAvailableException("Some media not available");
+        if (!allAvailable)
+            throw new MediaNotAvailableException("Some media not available");
     }
 
-    public CartItem checkMediaInCart(Media media){
+    public CartItem checkMediaInCart(Media media) {
         for (CartItem cartItem : lstCartItem) {
-            if (cartItem.getMedia().getId() == media.getId()) return cartItem;
+            if (cartItem.getMedia().getId() == media.getId())
+                return cartItem;
         }
         return null;
     }
